ac9bffe6cc7ad45db099d9b5c93c0b102f4501c0
Yash-Rawat-IMA <yash97181004@gmail.com>
2025-07-01 14:14:55 +0530

    Added another optimized function using shift operator apart frrom & and %, total 3 ways to find the total number of set bits in a number

--- Bitwise/count_SetBIts.cpp
+++ Bitwise/count_SetBIts.cpp
@@ -13,10 +13,21 @@ int countSetBits(int n)
     }
     return count;
 }
+int countSetBitsShift(int n)
+{
+    int c=0;
+    while (n)
+    {
+        if(n&1)
+            c++;
+        n>>=1;
+    }
+    return c;
+}
 int countSetBitsAnd(int n)
 {
     int res=0;
-    while(n>0)
+    while(n)
     {
         n=n&(n-1);
         res+=1;
@@ -30,6 +41,8 @@ int main()
     cin>>n;
     int res=countSetBits(n);
     cout<<res<<endl;
+    int res2=countSetBitsShift(n);
+    cout<<res2<<endl;
     int res1=countSetBitsAnd(n);
     cout<<res1<<endl;
     return 0;
